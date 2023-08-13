0749c57d8e5d646044830f8bf93d4889cdeb320a
Andy Ganse <andy@ganse.org>
2023-08-13 11:14:16 -0700

    rearranged structure and variable scopes

--- pygpt.py
+++ pygpt.py
@@ -6,6 +6,8 @@ Note you must have your OPENAI_API_KEY env var set.
 And you must be in an environment with the following python packages:
   * openai (for the open api)
   * rich (for the markdown/syntax-highlighting formatting)
+  * darkdetect (for putting code syntax or webpage into light/dark mode)
+  * beautifulsoup4 (for reading contents of urls)
 
 """
 
@@ -13,87 +15,246 @@ from cmd import Cmd
 import os
 import re
 import readline
+# import gnureadline as readline  # for macos
 import urllib
 
 from bs4 import BeautifulSoup
+import darkdetect
+import gradio as gr
 import openai
 from rich.console import Console
 from rich.markdown import Markdown
 
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
-code_theme = "default"  # "default" (for light), "monokai" (for dark)
-
-# set up the readline handling:
-if 'libedit' in readline.__doc__:
-    readline.parse_and_bind("bind ^I rl_complete")
-else:
-    readline.parse_and_bind("tab: complete")
-history_file = os.path.expanduser('~/.gpt_history')
-if os.path.exists(history_file):
-    readline.read_history_file(history_file)
-console = Console()
-
-# set basic behavior of the ai agent here
-messages = [
-    {"role": "system", "content": "The following is a conversation with "
-     "an AI assistant. The assistant is helpful, creative, clever, friendly. "
-     "Its answers are polite and friendly but brief, only rarely exceeding "
-     "a single paragraph when really necessary to explain a point. "
-     "The assistant labels all markdown code snippets with the code language. "
-     "Mathematical answers and expressions written by the assistant are "
-     "always formatted in unicode characters rather than latex, using full "
-     "mathematical notation rather than programming notation. "
-     "The assistant only very occasionally uses emojis to show enthusiasm."}
-]
 
 
-def generate_response(input, supplemental, model):
-    if input:
-        messages.append({"role": "user", "content": input})
-        if supplemental is not None:
-            messages.append({"role": "user", "content": supplemental})
-        metadata = {}
-        try:
-            chat = openai.ChatCompletion.create(
-                model=model, messages=messages
-            )
-            reply = chat.choices[0].message.content
-            messages.append({"role": "assistant", "content": reply})
-            metadata["prompt_tokens"] = chat.usage.prompt_tokens
-            metadata["completion_tokens"] = chat.usage.completion_tokens
-        except openai.OpenAIError as e:
-            errormsg = e.response["error"]["message"]
-            console.print(f"[bold red]OpenAI API Error:[/bold red] {errormsg}")
-            return ""
-        except Exception as e:
-            console.print(f"[bold red]An error occurred:[/bold red] {str(e)}")
-            return ""
+def submit_to_gpt(messages, model, temperature, top_p):
+
+    metadata = {}
+    try:
+        chat = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            top_p=top_p,
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        metadata["prompt_tokens"] = chat.usage.prompt_tokens
+        metadata["completion_tokens"] = chat.usage.completion_tokens
+    except openai.OpenAIError as e:
+        errormsg = e.response["error"]["message"]
+        print(f"OpenAI API Error: {errormsg}")
+        return ""
+    except openai.error.RateLimitError as e:
+        print(f"Sorry, hit a too-many-users limit: {e.reason}.")
+        return ""
+    except openai.error.InvalidRequestError as e:
+        print(f"Sorry, hit a too-long-submission limit: {e.reason}.")
+        return ""
+    except openai.error.Timeout as e:
+        print(f"Sorry, request apparently timed out: {e.reason}.")
+        return ""
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return ""
+
     return reply, metadata
 
 
-class GptInterpreter(Cmd):
-    model = "gpt-4"  # set this in one spot with default & cmdline arg
-    # model = "gpt-3.5-turbo"  # set this in one spot with default & cmdline arg
+def get_url_contents(url_search):
+
+    skip_input = False
+    url = url_search.group(1)
+    try:
+        maxchar = 15000
+        # values = {"name": "Michael Foord",
+        #           "location": "Northampton",
+        #           "language": "Python"}
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+        # resp = requests.get("https://www.embassy-worldwide.com", headers=headers)
+
+        # data = urllib.parse.urlencode(values)
+        # data = data.encode("ascii")
+        req = urllib.request.Request(url, headers=headers)
+        # req = urllib.request.Request(url, data, headers)
+        html = urllib.request.urlopen(req).read()
+        # html = urllib.request.urlopen(url).read()
+        webpagetext = BeautifulSoup(html, "html.parser").get_text()
+        webpagetext = webpagetext.replace("\n", " ")
+        orig_length_webpagetext = len(webpagetext)
+        if orig_length_webpagetext > maxchar:
+            user_input = input(
+                "Warning: length of webpagetext string "
+                f"= {len(webpagetext)} which will rapidly use up your "
+                f"tokens; also must be truncated to first {maxchar} "
+                "chars.  Continue?  (yN): ")
+            if user_input.lower() != "y" and user_input.lower() != "yes":
+                skip_input = True
+            else:
+                webpagetext = (
+                    "GPT please note that due to length, "
+                    f"webpage truncated to first {maxchar} characters,"
+                    f"about {maxchar/orig_length_webpagetext*100.0}%, "
+                    "which may affect your interpretation of it:\n"
+                    "------------------\n") + webpagetext
+                webpagetext = webpagetext[:maxchar]
+    except urllib.error.HTTPError as e:
+        print(f"Sorry, HTTP error: {e.code} in trying to access URL..")
+        # line = re.sub("<<.*>>", f"<<HTTP error {e.code}>>", line)
+        skip_input = True
+    except urllib.error.URLError as e:
+        print(f"Sorry, URL error: {e.reason} in trying to access URL.")
+        # line = re.sub("<<.*>>", f"<<URL error {e}>>", line)
+        skip_input = True
+    except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
+        skip_input = True
+
+    return webpagetext, skip_input
+
+
+def generate_response(input,
+                      messages=None,
+                      model=None,
+                      temperature=None,
+                      top_p=None):
+    """Check for input plugins (like webpage urls), retrieve them, submit input
+    to GPT model and return reply."""
+
+    # Set GPT submission options
+    if temperature is None:
+        temperature = 1
+    if top_p is None:
+        top_p = 1
+    if model is None:
+        model = "gpt-4"  # set this in one spot with default & cmdline arg
+        # model = "gpt-3.5-turbo"
+    if messages is None:
+        messages = [{
+            "role": "system", "content": "The following is a conversation with"
+            " an AI assistant. The assistant is helpful, creative, friendly. "
+            "Its answers are polite but brief, only rarely exceeding "
+            "a single paragraph when really necessary to explain a point. "
+            "The assistant labels all markdown code snippets with the code "
+            "language. Mathematical answers and expressions written by the "
+            "assistant are always formatted in unicode characters rather than "
+            "latex, using full mathematical notation rather than programming "
+            "notation. The assistant only very occasionally uses emojis to "
+            "show enthusiasm."
+        }]
+
+    # check for possible URL in user-submitted text
+    webpagetext = None
+    skip_input = False
+    url_search = re.search("<<(.*)>>", input, re.IGNORECASE)
+
+    if url_search:
+        webpagetext, skip_input = get_url_contents(url_search)
+
+    if not skip_input:
+
+        if input:
+            messages.append({"role": "user", "content": input})
+            if webpagetext is not None:
+                messages.append({"role": "user", "content": webpagetext})
+
+        reply, metadata = submit_to_gpt(messages,
+                                        model,
+                                        temperature,
+                                        top_p)
+
+    return reply, metadata
+
+
+class CmdLineInterpreter(Cmd):
+    """REPL command line interpreter based on built-in python Cmd package."""
+
+    # Set default CmdLinInterpreter interface options:
+    # prompt = "\x01\033[1m\x02Me:\x01\033[0m\x02 "  # bold only
     # prompt = "Me: "
     # prompt = "\x01\n\033[01;32m\x02Me:\x01\033[00m\x02 "  # color
-    prompt = "\x01\033[1m\x02Me:\x01\033[0m\x02 "  # bold only
-    # prompt = "\n\033[01;32m😃\033[37m\033[01;32m Me:\033[00m "
+    prompt = "\n\033[01;32m😃\033[37m\033[01;32m Me:\033[00m "
+    # gptprompt = "\x01\033[1m\x02GPT:\x01\033[0m\x02 "  # bold only
     # gptprompt = "GPT: "
     # gptprompt = "\x01\033[01;36m\x02GPT:\x01\033[00m\x02 "
-    gptprompt = "\x01\033[1m\x02GPT:\x01\033[0m\x02 "  # bold only
-    # gptprompt = "\033[01;32m🤖\033[37m\033[01;36m GPT:\033[00m "
+    gptprompt = "\033[01;32m🤖\033[37m\033[01;36m GPT:\033[00m "
     # gptprompt = "\n[bold blue]GPT[/bold blue]:  "  # use 'rich' formatting
+    intro = ""
+    allow_injections = True
+    if darkdetect.isDark():
+        code_theme = "monokai"
+    elif darkdetect.isLight():
+        code_theme = "default"
+    history_file = os.path.expanduser('~/.gpt_history')
 
-    def __init__(self):
+    # Set default GPT options (None means use the default in generate_response)
+    messages = None
+    model = None
+    temperature = None
+    top_p = None
+
+    console = Console()
+
+    def __init__(self,
+                 prompt=None,
+                 gptprompt=None,
+                 code_theme=None,
+                 intro=None,
+                 messages=None,
+                 model=None,
+                 temperature=None,
+                 top_p=None,
+                 allow_injections=None,
+                 history_file=None,
+                 ):
+
+        # If constructor args specified any of these variables, update defaults
+        if prompt is not None:
+            self.prompt = prompt,
+        if gptprompt is not None:
+            self.gptprompt = gptprompt,
+        if code_theme is not None:
+            self.code_theme = code_theme,
+        if intro is not None:
+            self.intro = intro,
+        if messages is not None:
+            self.messages = messages,
+        if model is not None:
+            self.model = model,
+        if temperature is not None:
+            self.temperature = temperature,
+        if top_p is not None:
+            self.top_p = top_p,
+        if allow_injections is not None:
+            self.allow_injections = allow_injections,
+
+        # Initialize the greeting/intro message on startup
         Cmd.__init__(self)
-        self.cmdloop(intro=f"Welcome to model {self.model}!\nNote you can "
-                     "enter page contents of a URL by putting the URL in "
-                     "double chevrons like this: <<URL>>\n")
+        if self.model is not None:
+            self.intro += f"model={self.model}  "
+        if self.temperature is not None:
+            self.intro += f"temp={self.temperature}  "
+        if self.top_p is not None:
+            self.intro += f"top_p={self.top_p}  "
+        if self.allow_injections:
+            self.intro += ("\nYou can enter page contents of a URL by putting "
+                           "the URL in double chevrons like this: <<URL>> ")
+        self.intro += "\n"
+        self.cmdloop(intro=self.intro)
+
+        # Set up the readline handling:
+        if 'libedit' in readline.__doc__:
+            readline.parse_and_bind("bind ^I rl_complete")
+        else:
+            readline.parse_and_bind("tab: complete")
+        if os.path.exists(self.history_file):
+            readline.read_history_file(self.history_file)
 
     def cmdloop(self, intro):
         try:
-            Cmd.cmdloop(self, intro)
+            Cmd.cmdloop(self, self.intro)
         except KeyboardInterrupt:
             print()
             self.do_exit()
@@ -105,7 +266,7 @@ class GptInterpreter(Cmd):
 
     def do_exit(self, line=None):
         print(" Ok, goodbye...")
-        readline.write_history_file(history_file)
+        readline.write_history_file(self.history_file)
         return True
 
     do_EOF = do_exit  # enables ctrl-D to exit
@@ -114,65 +275,41 @@ class GptInterpreter(Cmd):
         if line == "exit" or line == "quit" or line == "q":
             return self.do_exit()
 
-        # check for possible URL in user-submitted text
-        webpagetext = None
-        skip_input = False
-        url_search = re.search("<<(.*)>>", line, re.IGNORECASE)
-        if url_search:
-            url = url_search.group(1)
-            try:
-                maxchar = 15000
-                html = urllib.request.urlopen(url).read()
-                webpagetext = BeautifulSoup(html, "html.parser").get_text()
-                webpagetext = webpagetext.replace("\n", " ")
-                orig_length_webpagetext = len(webpagetext)
-                if orig_length_webpagetext > maxchar:
-                    user_input = input("Warning: length of webpagetext string "
-                        f"= {len(webpagetext)} which will rapidly use up your "
-                        f"tokens; also must be truncated to first {maxchar} "
-                        "chars.  Continue?  (yN): ")
-                    if user_input.lower() != "y" and user_input.lower() != "yes":
-                        skip_input = True
-                    else:
-                        webpagetext = ("GPT please note that due to length, "
-                            f"webpage truncated to first {maxchar} characters,"
-                            f"about {maxchar/orig_length_webpagetext*100.0}%, "
-                            "which may affect your interpretation of it:\n"
-                            "------------------\n") + webpagetext
-                        webpagetext = webpagetext[:maxchar]
-            except urllib.error.HTTPError as e:
-                print(f"Sorry, HTTP error: {e.code} in trying to access URL..")
-                line = re.sub("<<.*>>", f"<<HTTP error {e.code}>>", line)
-                skip_input = True
-            except urllib.error.URLError as e:
-                print(f"Sorry, URL error: {e.reason} in trying to access URL.")
-                line = re.sub("<<.*>>", f"<<URL error {e}>>", line)
-                skip_input = True
-            # let's also catch this error:
-            # openai.error.RateLimitError: That model is currently overloaded
-            # with other requests. You can retry your request, or contact us
-            # through our help center at help.openai.com if the error persists.
-            # (Please include the request ID 73ac0a3985379d57d5992b960d7ec24f
-            # in your message.)
+        reply, metadata = generate_response(line,
+                                            self.model,
+                                            self.messages,
+                                            self.temperature,
+                                            self.top_p)
 
-        if not skip_input:
-            reply, metadata = generate_response(line, webpagetext, self.model)
-
-            # handle markdown and syntax highlighting and word/line wrapping;
-            # technically could just use print() instead, just not as pretty:
-            console.print(f"[grey78][{metadata['prompt_tokens']} prompt-tokens; "
-                          "includes resubmission of all history this session plus "
-                          "page contents of any urls given...][/grey78]")
-            console.print(" ")
-            console.print(Markdown(self.gptprompt + reply, code_theme=code_theme))
-            console.print(f"[grey78][{metadata['completion_tokens']} "
-                          "completion-tokens just for this response...][/grey78]")
-            console.print(" ")
+        # handle markdown and syntax highlighting and word/line wrapping;
+        # technically could just use print() instead, just not as pretty:
+        self.console.print(
+            f"[grey78][{metadata['prompt_tokens']} prompt-tokens; "
+            "includes resubmission of all history this session plus "
+            "page contents of any urls given...][/grey78]")
+        self.console.print(" ")
+        self.console.print(
+            Markdown(self.gptprompt + reply, code_theme=self.code_theme)
+        )
+        self.console.print(
+            f"[grey78][{metadata['completion_tokens']} "
+            "completion-tokens just for this response...][/grey78]"
+        )
+        self.console.print(" ")
 
 
-def main():
-    GptInterpreter()
+def runChatWebApp():
+    """Start a gradio-based webapp for the chat interface"""
+
+    def chat(user_input):
+        return "You said: " + user_input
+
+    iface = gr.Interface(chat, "text", "text", title="My Chat App")
+    iface.launch()
 
 
 if __name__ == '__main__':
-    main()
+
+    CmdLineInterpreter()
+
+    # runChatWebApp()  # for gradio
